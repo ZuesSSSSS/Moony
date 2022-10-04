741dc871dc204095839ee168b9e86c1d57859350
David Mudriy <davidmudriy@gmail.com>
2022-10-03 19:44:51 -0700

    Svelte Init

---
+++ src/lib/Header.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<link rel='stylesheet' href='./header.css'>
+</svelte:head>
+
+<div>
+    <a id='logo'>Moony</a>
+    <nav class='header'>
+        <ul>
+            <li>
+                <a href='/' class='secondary'>Home</a>
+            </li>
+            <li>
+            </li>
+        </ul>
+    </nav>
+</div>
